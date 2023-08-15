738cd90f201fe046c6e2d5dad84aa1c71b3ef042
Mugara374 <m.mushi@alustudent.com>
2023-08-15 03:16:38 -0700

    Corrections

--- PLD_3/python.py
+++ PLD_3/python.py
@@ -1,5 +1,5 @@
 class JobPortal:
-    def _init_(self):
+    def __init__(self):
         self.job_listings = []
         self.training_courses = []
 
@@ -7,7 +7,8 @@ class JobPortal:
         self.job_listings.append({"title": title, "company": company, "location": location, "description": description})
 
     def add_training_course(self, title, provider, duration, description):
-        self.training_courses.append({"title": title, "provider": provider, "duration": duration, "description": description})
+        self.training_courses.append(
+            {"title": title, "provider": provider, "duration": duration, "description": description})
 
     def view_job_listings(self):
         print("==== Job Listings ====")
@@ -32,33 +33,39 @@ def display_menu():
     print("5. Exit")
     print("======================================")
 
-if __name__ == "_main_":
+
+def main():
     job_portal = JobPortal()
+    menu_functions = {
+        "1": job_portal.view_job_listings,
+        "2": job_portal.view_training_courses,
+        "3": job_portal.add_job_listing,
+        "4": job_portal.add_training_course
+    }
 
     while True:
         display_menu()
         choice = input("Enter your choice (1/2/3/4/5): ")
 
-        if choice == "1":
-            job_portal.view_job_listings()
-        elif choice == "2":
-            job_portal.view_training_courses()
-        elif choice == "3":
-            title = input("Enter the job title: ")
-            company = input("Enter the company name: ")
-            location = input("Enter the job location: ")
-            description = input("Enter the job description: ")
-            job_portal.add_job_listing(title, company, location, description)
-            print("Job listing added successfully!")
-        elif choice == "4":
-            title = input("Enter the course title: ")
-            provider = input("Enter the course provider: ")
-            duration = input("Enter the course duration: ")
-            description = input("Enter the course description: ")
-            job_portal.add_training_course(title, provider, duration, description)
-            print("Training course added successfully!")
-        elif choice == "5":
+        if choice == "5":
             print("Thank you for using our Job Portal and Training app. Goodbye!")
             break
+
+        if choice in menu_functions:
+            function = menu_functions[choice]
+            if choice in ["3", "4"]:
+                title = input("Enter the title: ")
+                provider = input("Enter the provider: ")
+                duration = input("Enter the duration: ")
+                description = input("Enter the description: ")
+                function(title, provider, duration, description)
+                print("Added successfully!")
+            else:
+                function()
         else:
             print("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
+
